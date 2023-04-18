59f03990202f99fa1024d805c6a6f4e79bb4bc56
Bruno Capistrano <bruno.capistrano@alana.ai>
2023-04-18 16:47:20 -0300

    delete cap4

--- cap4/exercicio3.py
+++
@@ -1,7 +0,0 @@
-num1 = 3
-num2 = 4
-num3 = 5
-
-arr = [num1,num2,num3]
-
-print("O maior valor é", max(arr), "e o menor é", min(arr),".")
--- cap4/exercicio4.py
+++
@@ -1,6 +0,0 @@
-salario = float(input("Digite o seu salário: "))
-
-if salario > 1250:
-    print("O seu salário será:", salario+(salario * (10/100)))
-if salario <= 1250:
-    print("O  seu salário será:", salario+(salario * (15/100)))
